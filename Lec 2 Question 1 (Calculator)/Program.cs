8f060a00d88d5026bed0ce5f9fdecb163d55e53e
Diyanafernando <135825998+Diyanafernando@users.noreply.github.com>
2024-02-25 21:28:33 +0530

    Add files via upload

---
+++ Lec 2 Question 1 (Calculator)/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec_2_Question_1__Calculator_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CALCULATOR");
+            Console.WriteLine("Enter the first number");
+            double num1 = 
+                Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine());
+            Operation(+,-,*,/)
+                Console.WriteLine("Enter the operation(+,-,*,/"):);
+            char operation =
+                Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Enter the second number : ");
+            double num2 =
+                Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+
+            switch (operation) 
+            {
+              case '+':
+                    result = num1 + num2;
+                    break;
+
+              case '-':
+                    result = num1 - num2;
+                    break;
+
+              case '*':
+                    result = num1 * num2;
+                    break;
+
+              case '/':
+                    if (num2 !== 0) ;
+                    result = num1 / num2;
+                    else
+                        Console.WriteLine("Error :Division by zero");
+              return;
+
+                default:
+
+                    Console.WriteLine("Invalid operation entered");
+                    return;
+                    
+            }
+        }
+    }
+}
---
+++ Lec 2 Question 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec_2_Question_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add numbers to the array");
+            int[] numbers = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i} {numbers[i+1]}:");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            Console.WriteLine("Numbers entered :");
+            foreach (int num in numbers)
+                Console.Write(num + " ");
+
+            int sum = 0;
+            foreach (int num in numbers)
+                sum += num;
+        }
+    }
+}
